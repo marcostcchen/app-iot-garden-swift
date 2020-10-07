beacb0f02b147e22b1e2cb97bc9901174059b6ff
Marcos Chen <marcostcchen@gmail.com>
2020-10-07 11:18:13 -0300

    Initial Commit

---
+++ app-iot-garden-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app-iot-garden-swift
+//
+//  Created by Imac21 on 07/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app-iot-garden-swift/app_iot_garden_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_iot_garden_swiftApp.swift
+//  app-iot-garden-swift
+//
+//  Created by Imac21 on 07/10/20.
+//
+
+import SwiftUI
+
+@main
+struct app_iot_garden_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
